c4db5b85a47da47544b73612241e22bb488f8e65
firmwarecostum <145313986+firmwarecostum@users.noreply.github.com>
2025-12-10 11:47:31 +0800

    Update proto_select.h

--- src/ucp/proto/proto_select.h
+++ src/ucp/proto/proto_select.h
@@ -209,7 +209,6 @@ void ucp_proto_select_short_init(ucp_worker_h worker,
                                  ucp_proto_select_t *proto_select,
                                  ucp_worker_cfg_index_t ep_cfg_index,
                                  ucp_worker_cfg_index_t rkey_cfg_index,
-                                 ucp_operation_id_t op_id, unsigned proto_flags,
                                  ucp_proto_select_short_t *proto_short);
 
 
